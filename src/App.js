8bf2401dd379f410556f2e6c5d0d54eb05ac8fbb
lopezrunco <lopezrunco@gmail.com>
2022-02-16 16:49:46 +0530

    Heading & buttons styles

--- src/App.js
+++ src/App.js
@@ -3,7 +3,21 @@ import './App.scss'
 function App() {
   return (
     <div className="App">
-      <h1>Entre mates y otras yerbas</h1>
+      <h1>Entre mates y otras yerbas h1</h1>
+      <h2>Entre mates y otras yerbas h2</h2>
+      <h3>Entre mates y otras yerbas h3</h3>
+      <h4>Entre mates y otras yerbas h4</h4>
+      <h5>Entre mates y otras yerbas h5</h5>
+      <h6>Entre mates y otras yerbas h6</h6>
+      <p>Entre mates y otras yerbas p</p>
+      <small>Entre mates y otras yerbas small</small>
+      <hr />
+      <button className='button sm-button primary-button'>Button</button>
+      <button className='button md-button primary-button'>Button</button>
+      <button className='button lg-button primary-button'>Button</button>
+      <button className='button sm-button secondary-button'>Button</button>
+      <button className='button md-button secondary-button'>Button</button>
+      <button className='button lg-button secondary-button'>Button</button>
     </div>
   )
 }
