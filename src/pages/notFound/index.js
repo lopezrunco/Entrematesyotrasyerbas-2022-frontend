3673dcc7c7a5910c671e293991801b7c2112a575
lopezrunco <lopezrunco@gmail.com>
2022-02-21 11:50:14 +0530

    NotFound page

--- src/pages/notFound/index.js
+++ src/pages/notFound/index.js
@@ -1,6 +1,44 @@
+import { EmojiDizzyFill } from 'react-bootstrap-icons'
+
+import PageTitle from '../../components/PageTitle'
+import NotFoundImg from '../../assets/404.png'
+import './style.scss'
+
 function NotFound() {
     return (
-        <h1>No encontrada</h1>
+        <>
+            <PageTitle
+                title='404!'
+                text='No ha sido posible cargar esta página'
+                imgClassName='general'
+            />
+            <main className="not-found-page">
+                <section className="container">
+                    <article className="row">
+
+                        <div className="col-lg-6">
+                            <div className='title'>
+                                <EmojiDizzyFill />
+                                <h2>Error 404!</h2>
+                            </div>
+                            <h4>La página que ha solicitado no se encuentra en el servidor</h4>
+                            <div className='links'>
+                                <h6>Pruebe mejor suerte en estos enlaces:</h6>
+                                <a href="/">Inicio</a>
+                                <a href="revista">Revista</a>
+                                <a href="novedades">Novedades</a>
+                                <a href="contacto">Contacto</a>
+                            </div>
+                        </div>
+
+                        <div className="col-lg-6">
+                            <img className='not-found-img' src={NotFoundImg} />
+                        </div>
+
+                    </article>
+                </section>
+            </main>
+        </>
     )
 }
 
